00f08011c71c8cf2cb8cdb08f965a1b491cc8ef5
MrNemod <131706032+MrNemod@users.noreply.github.com>
2024-05-30 15:20:41 -0600

    Update settings.py

--- Modular2/settings.py
+++ Modular2/settings.py
@@ -106,9 +106,5 @@ AUTH_USER_MODEL = 'PaginaCitas.CustomUser'
 # Pagina de login
 LOGIN_URL = '/login/'
 
-# Url static
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Backend autenticacion
 AUTHENTICATION_BACKENDS = ['PaginaCitas.auth_backends.EmailBackend', 'django.contrib.auth.backends.ModelBackend']
