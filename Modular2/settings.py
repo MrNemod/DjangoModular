31f09295cfd2eeae29934437b1711eaf4c7c7577
MrNemod <a12680@universidad-une.com>
2024-08-14 23:31:58 -0600

    Actualizado

--- Modular2/settings.py
+++ Modular2/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangomodular.onrender.com']
+ALLOWED_HOSTS = ['djangomodular.onrender.com', '127.0.0.1']
 
 # Application definition
 
